18304ed5f6cdb6debc3aaeae40a3433e8e4a7e74
RelevantShannon <shannon@relevanceai.com>
2025-12-07 18:25:09 +1100

    make cleaner

--- day/7/part2.sql
+++ day/7/part2.sql
@@ -9,42 +9,25 @@ WITH RECURSIVE beam (row_num, col_num, path_count)
 
                        UNION
 
-                       (WITH new_beam(row_num, col_num, path_count) AS
-                                 (SELECT beam.row_num + 1 AS row_num,
-                                         beam.col_num     AS col_num,
-                                         beam.path_count  AS path_count
-                                  FROM beam),
-                             new_beams(row_num, col_num, path_count)
-                                 AS (SELECT new_beam.row_num    AS row_num,
-                                            new_beam.col_num    AS col_num,
-                                            new_beam.path_count AS path_count
-                                     FROM new_beam
-                                              INNER JOIN board
-                                                         ON board.row_num =
-                                                            new_beam.row_num
-                                                             AND board.col_num =
-                                                                 new_beam.col_num
-                                     WHERE board.symbol = '.'
-
-                                     UNION ALL
-
-                                     SELECT new_beam.row_num   AS row_num,
+                       (WITH split_beams(row_num, col_num, path_count)
+                                 AS (SELECT beam.row_num + 1   AS row_num,
                                             split_beam.col_num AS col_num,
-                                            path_count         AS path_count
-                                     FROM new_beam
-                                              INNER JOIN board
-                                                         ON board.row_num =
-                                                            new_beam.row_num
-                                                             AND board.col_num =
-                                                                 new_beam.col_num,
-                                          LATERAL (VALUES (new_beam.col_num - 1),
-                                                          (new_beam.col_num + 1)) AS split_beam(col_num)
-                                     WHERE board.symbol = '^')
-                        SELECT new_beams.row_num                 AS row_num,
-                               new_beams.col_num                 AS col_num,
-                               SUM(new_beams.path_count)::bigint AS path_count
-                        FROM new_beams
-                        GROUP BY new_beams.row_num, new_beams.col_num))
+                                            beam.path_count    AS path_count
+                                     FROM beam
+                                              JOIN board
+                                                   ON board.row_num = beam.row_num + 1
+                                                       AND board.col_num = beam.col_num
+                                              CROSS JOIN LATERAL (
+                                         VALUES (CASE WHEN board.symbol = '.' THEN beam.col_num END),
+                                                (CASE WHEN board.symbol = '^' THEN beam.col_num - 1 END),
+                                                (CASE WHEN board.symbol = '^' THEN beam.col_num + 1 END)
+                                         ) AS split_beam(col_num)
+                                     WHERE split_beam.col_num IS NOT NULL)
+                        SELECT split_beams.row_num                 AS row_num,
+                               split_beams.col_num                 AS col_num,
+                               SUM(split_beams.path_count)::bigint AS path_count
+                        FROM split_beams
+                        GROUP BY split_beams.row_num, split_beams.col_num))
 SELECT beam.row_num, SUM(beam.path_count)
 FROM beam
 GROUP BY beam.row_num
